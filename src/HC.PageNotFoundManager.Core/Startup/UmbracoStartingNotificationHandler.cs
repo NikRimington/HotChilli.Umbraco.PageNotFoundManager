7ca7dc63e2a147f3191cf802bad3ed040f1878cc
Aaron <aaron@ph-creative.com>
2022-06-20 20:19:57 +0100

    More naming fixes

--- src/HC.PageNotFoundManager.Core/Startup/UmbracoStartingNotificationHandler.cs
+++ src/HC.PageNotFoundManager.Core/Startup/UmbracoStartingNotificationHandler.cs
@@ -4,36 +4,36 @@ using Umbraco.Cms.Core;
 using Umbraco.Cms.Core.Events;
 using Umbraco.Cms.Core.Migrations;
 using Umbraco.Cms.Core.Notifications;
-using Umbraco.Cms.Core.Scoping;
 using Umbraco.Cms.Core.Services;
 using Umbraco.Cms.Infrastructure.Migrations.Upgrade;
+using Umbraco.Cms.Infrastructure.Scoping;
 
 namespace HC.PageNotFoundManager.Core.Startup
 {
     public class UmbracoStartingNotificationHandler : INotificationHandler<UmbracoApplicationStartingNotification>
     {
-        private readonly ILogger<UmbracoStartingNotificationHandler> logger;
-        private readonly IRuntimeState runtimeState;
-        private readonly IScopeProvider scopeProvider;
-        private readonly IKeyValueService keyValueService;
-        private readonly IMigrationPlanExecutor migrationPlanExecutor;
+        private readonly ILogger<UmbracoStartingNotificationHandler> _logger;
+        private readonly IRuntimeState _runtimeState;
+        private readonly IScopeProvider _scopeProvider;
+        private readonly IKeyValueService _keyValueService;
+        private readonly IMigrationPlanExecutor _migrationPlanExecutor;
 
         public UmbracoStartingNotificationHandler(ILogger<UmbracoStartingNotificationHandler> logger,
             IRuntimeState runtimeState, IScopeProvider scopeProvider, IKeyValueService keyValueService, IMigrationPlanExecutor migrationPlanExecutor)
         {
             
-            this.logger = logger ?? throw new ArgumentNullException(nameof(logger));
-            this.runtimeState = runtimeState ?? throw new ArgumentNullException(nameof(runtimeState));
-            this.scopeProvider = scopeProvider ?? throw new ArgumentNullException(nameof(scopeProvider));
-            this.keyValueService = keyValueService ?? throw new ArgumentNullException(nameof(keyValueService));
-            this.migrationPlanExecutor = migrationPlanExecutor ?? throw new ArgumentNullException(nameof(migrationPlanExecutor));
+            this._logger = logger ?? throw new ArgumentNullException(nameof(logger));
+            this._runtimeState = runtimeState ?? throw new ArgumentNullException(nameof(runtimeState));
+            this._scopeProvider = scopeProvider ?? throw new ArgumentNullException(nameof(scopeProvider));
+            this._keyValueService = keyValueService ?? throw new ArgumentNullException(nameof(keyValueService));
+            this._migrationPlanExecutor = migrationPlanExecutor ?? throw new ArgumentNullException(nameof(migrationPlanExecutor));
         }
 
         public void Handle(UmbracoApplicationStartingNotification notification)
         {
-            if (runtimeState.Level < RuntimeLevel.Upgrade)
+            if (_runtimeState.Level < RuntimeLevel.Upgrade)
             {
-                logger.LogInformation("Umbraco Runtime is not Run/Upgrade mode, so a database connection is unlikely to be available for migrations");
+                _logger.LogInformation("Umbraco Runtime is not Run/Upgrade mode, so a database connection is unlikely to be available for migrations");
                 return;
             }
 
@@ -43,7 +43,7 @@ namespace HC.PageNotFoundManager.Core.Startup
         private void ApplyMigration()
         {
             var upgrader = new Upgrader(new Migrations.PageNotFoundMigrationPlan());
-            upgrader.Execute(migrationPlanExecutor, scopeProvider, keyValueService);
+            upgrader.Execute(_migrationPlanExecutor, _scopeProvider, _keyValueService);
         }
     }
 }
