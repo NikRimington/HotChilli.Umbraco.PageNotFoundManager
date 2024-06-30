60b8978afa4b0522af299947b77e295a9d9f038d
Aaron Sadler <aaron@umbhost.net>
2024-06-30 20:28:48 +0100

    Added a Notification for uSync to listed out for

---
+++ src/HC.PageNotFoundManager/Notifications/OnConfigurationSavedNotification.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections.Generic;
+using Umbraco.Cms.Core.Notifications;
+
+namespace HC.PageNotFoundManager.Notifications
+{
+    public class OnConfigurationSavedNotification : INotification
+    {
+        public IEnumerable<Models.PageNotFound>? Configuration { get; }
+
+        public OnConfigurationSavedNotification(IEnumerable<Models.PageNotFound>? configuration)
+        {
+            Configuration = configuration;
+        }
+    }
+}
